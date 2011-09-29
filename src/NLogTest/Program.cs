28ba0956c0efc3ebc2e1d97fdb4187d440cb5181
Thomas Fischer <thofisch@gmail.com>
2011-09-29 21:02:03 +0200

    Initial commit

---
+++ src/NLogTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using NLog;
+
+namespace NLogTest
+{
+	internal class Program
+	{
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+		private static void Main()
+		{
+			while( true )
+			{
+				Logger.Debug("Hello World from NLog");
+				Logger.Info("Hello World from NLog");
+				Logger.Warn("Hello World from NLog");
+				Logger.Error("Hello World from NLog");
+				Logger.Fatal("Hello World from NLog");
+
+				Thread.Sleep(10000);
+			}
+		}
+	}
+}
---
+++ src/log4netTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace log4netTest
+{
+	internal class Program
+	{
+		private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
+		private static void Main()
+		{
+			XmlConfigurator.Configure();
+
+			while( true )
+			{
+				Logger.Debug("Hello World from log4net");
+				Logger.Info("Hello World from log4net");
+				Logger.Warn("Hello World from log4net");
+				Logger.Error("Hello World from log4net");
+				Logger.Fatal("Hello World from log4net");
+
+				Thread.Sleep(10000);
+			}
+		}
+	}
+}
